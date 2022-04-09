00453cdaaa38e3e12b261c451ca726825e9ce758
Elshat <z90602190@hotmail.com>
2022-04-09 20:57:50 +0300

    work 3 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 
-    //everything going to be ok!
-    // just to press on!
+    //everything going to be ok! work1
+    // just to press on!         work2
+    // doing some work!          work3
+
 }
