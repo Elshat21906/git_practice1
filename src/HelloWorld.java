98659a2098306d17e675ef2357a44f06f04aafdd
Elshat <z90602190@hotmail.com>
2022-04-14 17:11:22 +0300

    14.04.2022

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,4 +4,8 @@ public class HelloWorld {
     // just to press on!         work2
     // doing some work!          work3
 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
 }
